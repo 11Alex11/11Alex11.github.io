bf5da720407de8af52f7fbd5d25269185773b1ae
Alex Wood <woodalex232@gmail.com>
2015-05-12 14:09:21 -0400

    First version of preloader complete, hover not working correctly

--- js/alexwood.js
+++ js/alexwood.js
@@ -1,11 +1,8 @@
 'use strict';
 window.addEventListener('load',function(){
+	$('body').toggleClass('loaded');
 	var logo = document.getElementsByClassName("logo")[0];
 	var rotate = 0;
-	window.setInterval(function(){
-		logo.style.rotate(20deg);
-	},1000);
-
 
 
 
