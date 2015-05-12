d330536d834a2256f2f1b2f43983234c7a824c59
Alex Wood <woodalex232@gmail.com>
2015-05-11 22:22:33 -0400

    Added logo/loading spinner, incomplete

---
+++ js/alexwood.js
@@ -0,0 +1,13 @@
+'use strict';
+window.addEventListener('load',function(){
+	var logo = document.getElementsByClassName("logo")[0];
+	var rotate = 0;
+	window.setInterval(function(){
+		logo.style.rotate(20deg);
+	},1000);
+
+
+
+
+
+});
