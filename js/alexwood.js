d1898536e42fffe9a1fbafba780e2842088f7b4d
Alex Wood <woodscraft6@hotmail.com>
2015-05-14 22:25:37 -0400

    Screen transitions work

--- js/alexwood.js
+++ js/alexwood.js
@@ -3,20 +3,31 @@ var LEFT = "LEFT";
 var RIGHT = "RIGHT";
 window.addEventListener('load',function(){
 	var header = $('.navigation-bar');
+	var headerWait=false;
 	$('body').toggleClass('loaded');
 	$('logo').toggleClass('logo');
 	var logo = document.getElementsByClassName("logo")[0];
 	var rotate = 0;
 	var content = $("information-wrapper");
 
+	var navTotal = header[0].children.length;
+	var curIndex=0;
+	var index=curIndex;
+
+	/*Each button is given an href and this will determine what button is clicked and open the href*/
+	/*Need to change to xml request*/
 	header[0].addEventListener("click",function(event){
-		if(event.target.classList.contains("home")){
-			window.open("./projects/CS349/A1/utrack.html");
-
-
+		if(headerWait==false){
+			var index = getIndex(event.target);
+			console.log(index);
+			if(index>curIndex){
+				nextPage(LEFT);
+			}
+			else if(index < curIndex){
+				nextPage(RIGHT);
+			}
+			curIndex=index;
 		}
-
-
 	});
 
 
@@ -27,37 +38,26 @@ window.addEventListener('load',function(){
 
 
 	nextPage(LEFT);
-
+	function getIndex(node){
+		var index=0;
+		while((node=node.previousElementSibling)!=null){
+			index++;
+		}
+		return index;
+	}
 	function nextPage(dir){
+		headerWait = true;
 		if(dir==LEFT){
 			$(".information-wrapper").toggleClass("move-left");
 		}
-		else{
+		else if(dir==RIGHT){
 			$(".information-wrapper").toggleClass("move-right");
-
 		}
-		//Interval from .move-left and .move-right
 		window.setTimeout(function(){
-			if(dir==LEFT){
-				$(".information-wrapper").toggleClass("move-left");
-				$(".information-wrapper").toggleClass("move-right-instant");
-				window.setTimeout(function(){
-			$(".information-wrapper").toggleClass("move-right-instant");
-			$(".information-wrapper").toggleClass("reset");
+			$(".information-wrapper").toggleClass("move-left",false);
+			$(".information-wrapper").toggleClass("move-right",false);
 
-		},0);
-				//$(".information-wrapper").toggleClass("toggle-invis");
-				//nextPage(LEFT);
-			}
-			else{
-				$(".information-wrapper").toggleClass("move-left");
-
-			}
-			window.setTimeout(function(){
-				$(".information-wrapper").toggleClass("reset");
-				nextPage(LEFT);
-			},2000);
-
+			headerWait=false;
 		},1000);
 	}
 });
